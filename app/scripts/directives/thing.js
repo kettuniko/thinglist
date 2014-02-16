58bb9f1fdc4e431c3b6c1444d013ed3d2ede6f2f
kopzu <nk.kopio@gmail.com>
2014-02-17 00:09:40 +0200

    Add own scope for thing-directive

--- app/scripts/directives/thing.js
+++ app/scripts/directives/thing.js
@@ -1,11 +1,12 @@
 'use strict';
 angular.module('thinglistApp')
     .directive('thing', function (Data) {
-      return {
-        templateUrl: 'partials/thing.html',
-        restrict: 'E',
-        link: function (scope, element, attrs) {
-          scope.thing = Data.query({type: attrs.type});
-        }
-      };
+        return {
+            templateUrl: 'partials/thing.html',
+            restrict: 'E',
+            scope: {},
+            link: function (scope, element, attrs) {
+                scope.thing = Data.query({type: attrs.type});
+            }
+        };
     });
